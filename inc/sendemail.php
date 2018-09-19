f1b5e9ffe96c8b9ad806bbcc5e4533a327627a8a
davidubanyi <ubanyi@gmail.com>
2018-09-19 10:20:02 +0100

    make some changes to the home page

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,10 +1,10 @@
 <?php
 
-    $to = "someone@yahoo.com";
+    $to = "contact@suissecreditng.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message from your Template";
+    $subject = "You have a message from $name";
 
     $fields = array();
     $fields{"name"}    = "Name";
